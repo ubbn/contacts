c9d414fad0529bf8b64505900ec639ef4b8df451
ubbn <ubbn@live.com>
2017-10-31 12:32:03 +0100

    Contact creation component

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import ImageInput from './ImageInput'
+import { Link } from 'react-router-dom'
+import serializeForm from 'form-serialize'
 
 export default class CreateContact extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let values = serializeForm(e.target, { hash: true })
+    // console.log(values)
+
+    if (this.props.onCreateContact)
+      this.props.onCreateContact(values)
+  }
+
   render(){
-    return <div>Create contact</div>
+    return (
+      <div>
+        <Link to='/' className='close-create-contact'>Back</Link>
+        <form className='create-contact-form' onSubmit={this.handleSubmit}>
+          <ImageInput 
+            className='create-contact-avatar-input'
+            name='avatarURL'
+            maxHeight={124}
+          />
+          <div className='create-contact-details'>
+            <input type="text" name='name' placeholder='Name'/>
+            <input type="text" name='email' placeholder='Email'/>
+            <button>Add Contact</button>
+          </div>
+        </form>
+      </div>
+    )
   }
 }
